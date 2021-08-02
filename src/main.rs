7dbdc86aa99c88b48c14b4e924473cebcdfff7a2
bhc8521 <bai_haochen@163.com>
2021-08-02 15:55:20 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -23,15 +23,3 @@ fn main() {
     let time = light.time_last();
     println!("{}", time);
 }
-
-fn sum(list: &[u32]) -> Option<u32> {
-    let mut sum:u32 = 0;
-    for item in list.iter() {
-        let res = sum.checked_add(*item);
-        sum = match res {
-            Some(v) => v,
-            None => return None
-        };
-    };
-    Some(sum)
-}
